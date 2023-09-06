392298f53c103c8238b5e390bcfa3a979b13fdb3
gamzemeryemkaya <gamzemeryemkaya@gmail.com>
2023-09-06 15:09:25 +0300

    kategori silme ve güncelleme işlemi tamamlandi

--- RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
@@ -12,25 +12,69 @@ namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
             _context = context;
         }
 
+        // (INSERT, UPDATE, DELETE) için ExecuteAsync
+        //QueryAsync, veritabanından veri çekmek için kullanılır.Result
         public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
         {
+            // Veritabanından tüm kategorileri çekmek için SQL sorgusu hazırlanır.
             string query = "Select * From Category";
+
+            // Veritabanı bağlantısı oluşturulur ve bu bağlantı sorguyu çalıştırır.
             using (var connection = _context.CreateConnection())
             {
+                // Sorgu sonuçları, ResultCategoryDto türüne dönüştürülüp listeye çevrilir ve geri döndürülür.
                 var values = await connection.QueryAsync<ResultCategoryDto>(query);
                 return values.ToList();
             }
         }
 
+        /// <summary>
+        /// Yeni bir kategori oluşturur.
+        /// </summary>
+
         public async void CreateCategory(CreateCategoryDto categoryDto)
         {
+            //Yeni bir kategori eklerken veritabanına kategori adı ve durumu ekler.
             string query = "insert into Category (CategoryName,CategoryStatus) values (@categoryName,@categoryStatus)";
+            //Sorgu için parametreler oluşturulur ve kategori bilgileri eklenir.
             var parameters = new DynamicParameters();
             parameters.Add("@categoryName", categoryDto.CategoryName);
             parameters.Add("@categoryStatus", true);
+            // Veritabanı bağlantısı oluşturulur ve sorgu çalıştırılır.
             using (var connection = _context.CreateConnection())
             {
                 await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async void DeleteCategory(int id)
+        {
+            string query = "Delete From Category Where CategoryID=@categoryID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@categoryID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        /// <summary>
+        /// Belirtilen kategori bilgileri ile mevcut bir kategoriyi günceller.
+        /// </summary>
+
+         public async void UpdateCategory(UpdateCategoryDto categoryDto)
+        {
+            //Belirtilen kategori ID'sine sahip kategoriyi günceller.
+
+            string query = "Update Category Set CategoryName=@categoryName,CategoryStatus=@categoryStatus where CategoryID=@categoryID";
+            //Sorgu için parametreler oluşturulur ve güncelleme için gerekli bilgiler eklenir.
+            var parameters = new DynamicParameters();
+            parameters.Add("@categoryName", categoryDto.CategoryName);
+            parameters.Add("@categoryStatus", categoryDto.CategoryStatus);
+            parameters.Add("@categoryID", categoryDto.CategoryID);
+            using (var connectiont = _context.CreateConnection())
+            {
+                await connectiont.ExecuteAsync(query, parameters);
             }
         }
     }
--- RealEstate_Dapper_Api/Repositories/CategoryRepository/ICategoryRepository.cs
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -4,7 +4,13 @@ namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
 {
     public interface ICategoryRepository
     {
+        // 'result' ve 'update' DTO'ları aynıdır.
+        // İki farklı DTO tanımlaması, temiz kod prensipleri açısından daha iyidir.
+        // Yönetim ve revizyon süreçlerinde hata kontrolünü daha kolay ve hızlı bir şekilde sağlamamıza yardımcı olur.
         Task<List<ResultCategoryDto>> GetAllCategoryAsync();
         void CreateCategory(CreateCategoryDto categoryDto);
+        void DeleteCategory(int id);
+
+        void UpdateCategory(UpdateCategoryDto categoryDto);
     }
 }
